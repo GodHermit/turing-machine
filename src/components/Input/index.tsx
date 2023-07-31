ec69153dde1bf580810aa8cc0b9fc064260811a4
Oleh Proidakov <first.god.hermit@gmail.com>
2023-07-31 15:02:59 +0300

    Add props to invalidate the `Input` component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,21 +1,28 @@
 'use client';
 
-import { ChangeEvent, useState, useId } from 'react';
+import clsx from 'clsx';
+import { ChangeEvent, useEffect, useId, useState } from 'react';
 
 interface InputProps {
 	label?: string;
 	value?: string;
-	onChange?: (value: string) => void;
+	isInvalid?: boolean;
+	invalidFeedback?: string;
+	onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
 export default function Input(props: InputProps) {
 	const id = useId();
 	const [value, setValue] = useState(props.value || '');
 
+	useEffect(() => {
+		setValue(props.value || '');
+	}, [props.value]);
+
 	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
 		const value = e.target.value;
 		setValue(value);
-		props.onChange?.(value);
+		props.onChange?.(e);
 	};
 
 	return (
@@ -23,16 +30,29 @@ export default function Input(props: InputProps) {
 			{props.label && (
 				<label htmlFor={id} className='form-label'>{props.label}</label>
 			)}
-			<div className='input-group'>
+			<div className={
+				clsx(
+					'input-group',
+					props.isInvalid && 'is-invalid',
+				)
+			}>
 				<input
 					type='text'
 					id={id}
-					className='form-control'
+					className={clsx(
+						'form-control',
+						props.isInvalid && 'is-invalid',
+					)}
 					value={value}
 					onChange={handleChange}
 				/>
 				<span className='input-group-text'>{value.length}</span>
 			</div>
+			{props.invalidFeedback && (
+				<div className='invalid-feedback position-absolute w-auto'>
+					{props.invalidFeedback}
+				</div>
+			)}
 		</>
 	);
 }
