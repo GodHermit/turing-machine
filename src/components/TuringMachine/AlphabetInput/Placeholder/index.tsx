f1148fa84055f35a17b3e6195a5880ecd8e0bfc2
Oleh Proidakov <first.god.hermit@gmail.com>
2023-08-23 16:37:01 +0300

    Add `AlpahbetInputPlaceholder` component
    Partially resolves: Add placeholder elements on page load #27

---
+++ src/components/TuringMachine/AlphabetInput/Placeholder/index.tsx
@@ -0,0 +1,11 @@
+import InputPlaceholder from '@/components/Input/Placeholder';
+
+export default function AlpahbetInputPlaceholder() {
+	return (
+		<div className='mb-4'>
+			<InputPlaceholder
+				label='Alphabet:'
+			/>
+		</div>
+	);
+}
